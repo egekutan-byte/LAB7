eb76e63d73c7573a3a10d85eef5185a60babbec3
Ege Kutan <ege.kutan@gmail.com>
2025-11-25 09:45:23 +0300

    Scenario1

---
+++ StudentDemo.java
@@ -0,0 +1,32 @@
+public class StudentDemo {
+    public static void main(String[] args) {
+        Student student = new Student("Ege",18);
+        student.printStudentInfo();
+    }
+}
+class Student {
+    public String name;
+    public int age;
+    public Student(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    public Student(){
+        this.name = "none";
+        this.age = 67;
+    }
+    
+    public void printStudentInfo(){
+        System.out.println(name);
+        System.out.println(age);
+    }
+
+
+}
+/*3.1-)since ı assign the values of ege to string and 18 to int ı have the result like this: Ege
+                                                                                           18   */
+//3.2-)NO ERRORS
+
+
+/*4-)There are 2 parameters in the public Student constructor thats why there
+are no errors in the main method otherwise would have a "no arguments found" error.*/
